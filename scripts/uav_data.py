1944c38152ccc0f4abe4f0d98fd96443fa55e3e5
Elton Vivot <eltonvivot@gmail.com>
2022-04-26 20:05:41 -0300

    added messages to script

--- scripts/uav_data.py
+++ scripts/uav_data.py
@@ -3,16 +3,19 @@ import argparse, requests
 API_ENDPOINT = "http://10.10.21.10:5001/uav_data"
 
 def get():
+    print("Method: GET")
     r = requests.get(url=API_ENDPOINT)
     print(r.json())
 
 def post(args):
+    print(f"Method: POST")
     if not args.cam_status:
         print("Missing '--cam-status' parameter.")
         return
     data = {
         'cam_status': args.cam_status
     }
+    print(f"Data:\n{data}")
     r = requests.post(url=API_ENDPOINT, data=data)
     print(r.json())
 
