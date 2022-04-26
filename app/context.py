14a9f54e3c5d075864ad31173bf54d94f086301e
Elton Vivot <eltonvivot@gmail.com>
2022-04-26 20:53:50 -0300

    fixed bugs

--- app/context.py
+++ app/context.py
@@ -2,11 +2,12 @@ import json
 from config import cdata
 
 def get_all():
-    return json.loads(cdata)
+    with open(cdata, 'r') as cfile:
+        return json.loads(cfile)
 
 def get(data_name):
     print(f"debug - get({data_name})")
-    with open(cdata, "r") as cfile:
+    with open(cdata, 'r') as cfile:
         data = json.loads(cfile)[data_name]
     if data_name not in data: print(f"Context do not have '{data_name}'.")
     return data[data_name]
@@ -16,5 +17,5 @@ def post(data_name, data):
     print(f"debug - post({data_name})")
     context = get_all()
     context[data_name] = data
-    with open(cdata, "w") as cfile:
+    with open(cdata, 'w') as cfile:
         json.dump(context, cfile, indent=2)
