3dd16293ec3151955ef8553fd12d303ebe0a6d98
Elton Vivot <eltonvivot@gmail.com>
2022-04-26 20:28:14 -0300

    activated debug mode

--- app/app.py
+++ app/app.py
@@ -9,4 +9,4 @@ if __name__ == '__main__':
     # endpoints
     app.register_blueprint(uav_data_bp)
     app.register_blueprint(ai_data_bp)
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5001, debug=True)
