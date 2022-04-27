3adeeb744b728cde5fb5446442855a3d40c4b78b
Elton Vivot <eltonvivot@gmail.com>
2022-04-26 21:46:29 -0300

    fixed bug

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 import logging
 
 # init logs
-logger = logging.getLogger(__name__)
 f='%(asctime)s | %(levelname)s | %(name)s:%(funcName)s | %(message)s'
 log_level=logging.DEBUG
+logging.basicConfig()
 logging.basicConfig(level=log_level, format=f)
