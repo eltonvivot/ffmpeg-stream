cb8c7078efd8aac3d8c9953c3a48079c3ecfb793
Elton Vivot <eltonvivot@gmail.com>
2022-04-26 21:08:16 -0300

    fixed bugs

--- scripts/uav_data.py
+++ scripts/uav_data.py
@@ -3,21 +3,21 @@ import argparse, requests
 API_ENDPOINT = "http://10.10.21.10:5001/uav_data"
 
 def get():
-    logger.debug("Method: GET")
+    print("Method: GET")
     r = requests.get(url=API_ENDPOINT)
-    logger.debug(r.json())
+    print(r.json())
 
 def post(args):
-    logger.debug(f"Method: POST")
+    print(f"Method: POST")
     if not args.cam_status:
-        logger.debug("Missing '--cam-status' parameter.")
+        print("Missing '--cam-status' parameter.")
         return
     data = {
         'cam_status': args.cam_status.lower()
     }
-    logger.debug(f"Data:\n{data}")
+    print(f"Data:\n{data}")
     r = requests.post(url=API_ENDPOINT, data=data)
-    logger.debug(r.json())
+    print(r.json())
 
 def main():
     parser = argparse.ArgumentParser(description="UAV_DATA REST Functions")
