57066851e632138e8c24d7c8f853afb90fb9182f
Elton Vivot <eltonvivot@gmail.com>
2022-05-03 17:21:19 -0300

    added plot_figure to flask request

--- app/ai_app.py
+++ app/ai_app.py
@@ -1,7 +1,7 @@
 # Simulates Object Detection rApp
 from flask import Blueprint, request, jsonify, g
 from context import get, post
-from ai_workflow import start_detection, stop_detection
+from ai_workflow import start_detection, stop_detection, plot_figure
 import threading
 
 ai_app_bp = Blueprint('ai_app', '__name__')
@@ -20,6 +20,7 @@ def handle_start_detection():
     # threading.Thread(target=start_detection).start()
     # threading.Thread(target=stop_detection, args=(ai_dtime,)).start()
     start_detection()
+    plot_figure(True, False, g.results)
     return jsonify({'result': g.results})
 
 @ai_app_bp.route('/ai_detection/stop', methods=['GET'])
--- app/ai_workflow.py
+++ app/ai_workflow.py
@@ -107,11 +107,11 @@ def plot_figure(should_save, should_display, results):
 
     line6, = ax.plot(times, [int(result['ap']) for result in results], label="Person's Average Precision (%)",
                      color=color1, marker='o', markersize=4)
-    line7, = ax.plot(times, [int(result['delay']) for result in results], label="UE network latency (ms)",
+    line7, = ax.plot(times, [int((result['delay'])[:-2]) for result in results], label="UE network latency (ms)",
                      color=color2, marker='o', markersize=4)
-    # line8, = ax.plot(times, [int(result['rate']) for result in results], label="UE network bandwidth (Mbps)",
+    # line8, = ax.plot(times, [int((result['rate'][:-4])) for result in results], label="UE network bandwidth (Mbps)",
     #                  color=color3, marker='o', markersize=4)
-    line9, = ax.plot(times, [result['loss'] for result in results], label="UE network packet loss (%)",
+    line9, = ax.plot(times, [float((result['loss'])[:-1]) for result in results], label="UE network packet loss (%)",
                      color=color4, marker='o', markersize=4)
     # line0, = ax.plot(times, [i * 3.6 for i in Cons_anel_n5_l1], label='H2 Root',
     #                  color=color5, marker='o', markersize=4)
