7141542f6d1b17e200d1e4c56b618cf91e0fb771
Elton Vivot <eltonvivot@gmail.com>
2022-05-12 11:49:02 -0300

    changed auto rules time to 1s

--- app/ai_workflow.py
+++ app/ai_workflow.py
@@ -79,9 +79,8 @@ def auto_rules(detection_name, change_rate, change_loss, stime, dec_time, inc_ti
                 max_loss = 2.5
                 min_loss = 0.0
 
-        interval = random.uniform(1.0, 2.0)
-        time.sleep(interval)
-        timer+=interval
+        time.sleep(1)
+        timer+=1
         # random
         rules = {}
         rules['rate'] = f"{random.uniform(lrate-30 if lrate-30 > min_rate else min_rate, lrate+30 if lrate+30 < max_rate else max_rate)}Mbps"
