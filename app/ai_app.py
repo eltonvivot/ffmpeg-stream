0c5589b378dfde5bd1523621705150e56fc58772
Elton Vivot <eltonvivot@gmail.com>
2022-05-14 00:31:48 -0300

    added some logs

--- app/ai_app.py
+++ app/ai_app.py
@@ -1,4 +1,5 @@
 # Simulates Object Detection rApp
+from asyncio.log import logger
 from flask import Blueprint, request, jsonify, g
 from context import get, post
 from ai_workflow import start_detection, stop_detection, plot_figures
@@ -17,15 +18,20 @@ def handle_uav_data():
 
 @ai_app_bp.route('/ai_detection/start', methods=['POST'])
 def handle_start_detection():
+    logger.info("Received Start Detection")
     g.results = {}
     g.inc_time = {}
     g.dec_time = {}
     first_name = 'first'
     second_name = 'second'
+    logger.info(f"Starting Detection '{first_name}' ...")
     start_detection(first_name, True, False, False)
+    logger.debug(f"Waiting 40s ...")
     time.sleep(40)
+    logger.info(f"Starting Detection '{second_name}' ...")
     start_detection(second_name, False, True, False)
     # new plot
+    logger.info(f"Plotting Figures ...")
     plot_figures(True, False, first_name, second_name)
     return jsonify({'result': "Ok."})
 
