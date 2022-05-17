23df57d630b77799ec08cd781fa8432b8352a350
Elton Vivot <eltonvivot@gmail.com>
2022-05-17 14:33:55 -0300

    changed test net parameters

--- app/ai_workflow.py
+++ app/ai_workflow.py
@@ -68,13 +68,13 @@ def auto_rules(detection_name, change_rate, change_loss, change_delay, stime, de
             apply_rules = True
             logger.info("------- DECREASING ----------")
             if change_rate:
-                max_rate = 3
-                min_rate = 2
+                max_rate = 2
+                min_rate = 1
                 lrate = 1.2
             if change_loss:
-                max_loss = 25.0
-                min_loss = 20.0
-                lloss = 22
+                max_loss = 30.0
+                min_loss = 25.0
+                lloss = 27
             if change_delay:
                 max_delay = 250
                 min_delay = 100
