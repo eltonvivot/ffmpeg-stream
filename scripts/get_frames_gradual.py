35e7c5d56f0271496192367151565ca9e98182a1
root <root@node21-10.grid.orbit-lab.org>
2022-08-16 14:49:00 +0000

    adding modifications

--- scripts/get_frames_gradual.py
+++ scripts/get_frames_gradual.py
@@ -10,28 +10,31 @@ ai_pass = "ffmpeg"
 rtsp_trans = ""
 cont_name = "uav-cam"
 
+def logf(string):
+    file = "/root/ffmpeg-stream/get_frames_gradual.log"
+    os.system(f"echo '{string}' >> {file}")
+
 def get_frame(last_name, new_name):
     print(f"\n------- GETTING FRAME {new_name} -------")
     print("Changing docker-compose ...")
     os.system(f"sed -i 's/{last_name}/{new_name}/g' {docker_file}")
-    time.sleep(1)
+    time.sleep(2)
 
     print("Starting docker container ...")
     os.system(f"docker-compose -f {docker_file} up -d --build {cont_name}")
     
-    print("Waiting 5s ...")
-    time.sleep(5)
+    print("Wainting 5s ...")
+    time.sleep(7)
 
     print("Getting frame ...")
     threading.Thread(target=os.system, args=(f"sshpass -p {ai_pass} ssh {ai_user}@{ai_ip} 'cd /root/frames && ./get_frame.sh {new_name} &>/dev/null &'", )).start()
-    print("Waiting 10s ...")
-    time.sleep(10)
+    print("Wainting 5s ...")
+    time.sleep(7)
 
     print("Stopping docker container ...")
     os.system(f"docker stop {cont_name}")
-    time.sleep(1)
+    time.sleep(2)
     print("Done.")
-    print(os.system(f"mv /root/ffmpeg-stream/video-frames/{new_name}.mp4 /root/dataset_images/"))
 
 if __name__ == '__main__':
 
@@ -39,8 +42,17 @@ if __name__ == '__main__':
     onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f)) and '.mp4' in f]
     onlyfiles.sort()
     last = '0001'
+    count = 0
+    ctrl = 5
+    os.system(f"python3 /root/ffmpeg-stream/scripts/tc_controller.py -m post --delay 0.1ms --loss {ctrl}% --rate 500.0Mbps")
     for f in onlyfiles:
+        if count > 15:
+            count = 1
+            ctrl += 5
+            os.system(f"python3 /root/ffmpeg-stream/scripts/tc_controller.py -m post --delay 0.1ms --loss {ctrl}% --rate 500.0Mbps")
+        logf(f" - {last} | {count} - {ctrl}")
         get_frame(last, f[:-4])
         last = f[:-4]
+        count += 1
     
     os.system(f"sed -i 's/{last}/0001/g' {docker_file}")
